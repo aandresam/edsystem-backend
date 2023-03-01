778206fd31af642324967498a3e4157079560526
German Aguirre <aandres060916@gmail.com>
2023-03-01 00:02:13 -0500

    Actualizando controlador '/authenticate' para devolver un ResponseEntity<?> con el token en el header en lugar del body

--- src/main/java/com/german/edsystem/controllers/AuthenticationController.java
+++ src/main/java/com/german/edsystem/controllers/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.german.edsystem.controllers;
 import com.german.edsystem.config.jwt.AuthenticationCredentials;
 import com.german.edsystem.config.jwt.JWTService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+
+
 
 @RestController
 @RequestMapping("/authenticate")
@@ -21,13 +25,16 @@ public class AuthenticationController {
     private final JWTService jwtService;
 
     @PostMapping
-    public String authenticate(@RequestBody AuthenticationCredentials credentials) {
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationCredentials credentials) {
         Authentication authentication = authenticationManager
                         .authenticate(new UsernamePasswordAuthenticationToken(credentials.getUsername(),
                                                                             credentials.getPassword()));
         if (!authentication.isAuthenticated()) {
             throw new UsernameNotFoundException("Credenciales incorectas");
         }
-        return jwtService.generateToken(credentials.getUsername());
+        String token = jwtService.generateToken(credentials.getUsername());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+        return ResponseEntity.ok().headers(headers).build();
     }
 }
